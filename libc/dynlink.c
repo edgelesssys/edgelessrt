f19e256f4d67415ecdb33643a95dec99328a63ce
Thomas Tendyck <tt@edgeless.systems>
2020-01-10 14:43:29 +0100

    dlsym

---
+++ libc/dynlink.c
@@ -0,0 +1,123 @@
+// Copyright (c) Edgeless Systems GmbH.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <elf.h>
+#include <openenclave/internal/globals.h>
+#include <openenclave/internal/trace.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Here we implement some dynlink functions. However, we do not actually implement
+dynamic linking. If an app tries to load a shared object, we pretend success and
+try to find all symbols in the statically linked enclave.
+*/
+
+void* dlopen(const char* file, int mode)
+{
+    return (void*)1; // just return any non-NULL value
+}
+
+// copied from musl/ldso/dynlink.c
+static uint32_t gnu_hash(const char* s0)
+{
+    const unsigned char* s = (void*)s0;
+    uint_fast32_t h = 5381;
+    for (; *s; s++)
+        h += h * 32 + *s;
+    return h;
+}
+
+// adapted from musl/ldso/dynlink.c
+static const Elf64_Sym* gnu_lookup(
+    uint32_t h1,
+    const uint32_t* hashtab,
+    const char* strings,
+    const Elf64_Sym* syms,
+    const char* s)
+{
+    uint32_t nbuckets = hashtab[0];
+    const uint32_t* buckets = hashtab + 4 + hashtab[2] * (sizeof(size_t) / 4);
+    uint32_t i = buckets[h1 % nbuckets];
+
+    if (!i)
+        return 0;
+
+    const uint32_t* hashval = buckets + nbuckets + (i - hashtab[1]);
+
+    for (h1 |= 1;; i++)
+    {
+        uint32_t h2 = *hashval++;
+        if ((h1 == (h2 | 1)) && !strcmp(s, strings + syms[i].st_name))
+            return syms + i;
+        if (h2 & 1)
+            break;
+    }
+
+    return 0;
+}
+
+const void* __dlsym(void* handle, const char* symbol)
+{
+    if (!symbol)
+        abort();
+
+    const uint8_t* const base = __oe_get_enclave_base();
+    assert(base);
+    const Elf64_Ehdr* const ehdr = __oe_get_enclave_elf_header();
+    assert(ehdr);
+    assert(ehdr->e_phoff);
+    const Elf64_Phdr* const phdr = (Elf64_Phdr*)(base + ehdr->e_phoff);
+
+    // find address of dynamic section in program header
+    const Elf64_Dyn* dyn = NULL;
+    for (int i = 0; i < ehdr->e_phnum; ++i)
+        if (phdr[i].p_type == PT_DYNAMIC)
+        {
+            assert(phdr[i].p_vaddr);
+            dyn = (Elf64_Dyn*)(base + phdr[i].p_vaddr);
+            break;
+        }
+    assert(dyn);
+
+    const char* strtab = NULL;
+    const Elf64_Sym* symtab = NULL;
+    const uint32_t* hashtab = NULL;
+
+    // find addresses of string, symbol, and symbol hash table in dynamic
+    // section
+    for (; dyn->d_tag != DT_NULL; ++dyn)
+        switch (dyn->d_tag)
+        {
+            case DT_STRTAB:
+                strtab = (char*)(base + dyn->d_un.d_ptr);
+                break;
+            case DT_SYMTAB:
+                symtab = (Elf64_Sym*)(base + dyn->d_un.d_ptr);
+                break;
+            case DT_GNU_HASH:
+                hashtab = (uint32_t*)(base + dyn->d_un.d_ptr);
+                break;
+        }
+
+    assert(strtab);
+    assert(symtab);
+    assert(hashtab);
+
+    // look up symbol
+    const Elf64_Sym* const sym =
+        gnu_lookup(gnu_hash(symbol), hashtab, strtab, symtab, symbol);
+
+    if (sym && sym->st_value)
+        return base + sym->st_value;
+
+    OE_TRACE_WARNING("Symbol not found: %s", symbol);
+
+    void __dl_seterr(const char*, ...);
+    __dl_seterr("Symbol not found: %s", symbol);
+
+    return NULL;
+}
---
+++ tests/dynlink/host/host.cpp
@@ -0,0 +1,29 @@
+#include <openenclave/host.h>
+#include <openenclave/internal/tests.h>
+#include <iostream>
+#include "test_u.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Usage: " << argv[0] << " ENCLAVE\n";
+        return EXIT_FAILURE;
+    }
+
+    const uint32_t flags = oe_get_create_flags();
+    oe_enclave_t* enclave = nullptr;
+
+    OE_TEST(
+        oe_create_test_enclave(
+            argv[1], OE_ENCLAVE_TYPE_AUTO, flags, nullptr, 0, &enclave) ==
+        OE_OK);
+    OE_TEST(test_ecall(enclave) == OE_OK);
+    OE_TEST(oe_terminate_enclave(enclave) == OE_OK);
+
+    cout << "=== passed all tests (" << argv[0] << ")\n";
+
+    return EXIT_SUCCESS;
+}
