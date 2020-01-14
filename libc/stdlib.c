7c09dba3f8bfbfdfd635c995c787466fedd0e807
Robert Gawlik <rg@edgeless.systems>
2020-01-14 12:59:31 +0100

    move __realpath_chk to libc/stdlib.c (#PR22)

--- libc/stdlib.c
+++ libc/stdlib.c
@@ -1,6 +1,7 @@
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
+#include <limits.h>
 #include <locale.h>
 #include <openenclave/enclave.h>
 #include <stdio.h>
@@ -20,4 +21,16 @@ unsigned long long strtoull_l(
 {
     OE_UNUSED(loc);
     return strtoull(nptr, endptr, base);
+}
+
+// http://refspecs.linux-foundation.org/LSB_4.0.0/LSB-Core-generic/LSB-Core-generic/libc---realpath-chk-1.html
+// https://github.com/bminor/glibc/blob/master/debug/realpath_chk.c
+char* __realpath_chk(const char* buf, char* resolved, size_t resolvedlen)
+{
+    if (resolvedlen < PATH_MAX)
+    {
+        //__chk_fail ();
+        abort();
+    }
+    return realpath(buf, resolved);
 }
