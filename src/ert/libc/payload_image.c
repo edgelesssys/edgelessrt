708881505a623d84c3502f94f26429acb7b711bf
Thomas Tendyck <tt@edgeless.systems>
2021-02-23 18:09:56 +0100

    AB#626 add payload data as measured pages
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/ert/libc/payload_image.c
+++ src/ert/libc/payload_image.c
@@ -4,5 +4,4 @@
 // these are set when the host loads and patches the enclave image
 OE_EXPORT volatile uint64_t _payload_reloc_rva;
 OE_EXPORT volatile uint64_t _payload_reloc_size;
-OE_EXPORT volatile uint64_t _payload_data;
 OE_EXPORT volatile uint64_t _payload_data_size;
