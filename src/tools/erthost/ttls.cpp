9d9e8b63e395774fbf1bc2a5f515d79e1df804f9
Leonard Cohnen <lc@edgeless.systems>
2021-03-19 12:58:47 +0100

    AB#647 integrate ttls
    Signed-off-by: Leonard Cohnen <lc@edgeless.systems>

--- src/tools/erthost/ttls.cpp
+++ src/tools/erthost/ttls.cpp
@@ -2,20 +2,8 @@
 #include <openenclave/internal/syscall/hook.h>
 #include <sys/socket.h>
 #include <sys/syscall.h>
+#include <ttls/ttls.h>
 #include <cassert>
-
-// TODO remove this dummy
-namespace edgeless::ttls
-{
-class Socket
-{
-  public:
-    virtual int Connect(
-        int sockfd,
-        const sockaddr* addr,
-        socklen_t addrlen) = 0;
-};
-} // namespace edgeless::ttls
 
 using namespace edgeless;
 
@@ -23,22 +11,66 @@ extern "C" long oe_syscall(long number, ...);
 
 namespace
 {
-class RawSocket final : public ttls::Socket
+class OESocket final : public ttls::Socket
 {
   public:
     int Connect(int sockfd, const sockaddr* addr, socklen_t addrlen) override
     {
         return oe_syscall(SYS_connect, sockfd, addr, addrlen);
     }
+    ssize_t Send(int sockfd, const void* buf, size_t len, int /*flags*/)
+        override
+    {
+        return oe_syscall(SYS_write, sockfd, buf, len);
+    }
+    ssize_t Recv(int sockfd, void* buf, size_t len, int /*flags*/) override
+    {
+        return oe_syscall(SYS_read, sockfd, buf, len);
+    }
+    int Shutdown(int fd, int how) override
+    {
+        return oe_syscall(SYS_shutdown, fd, how);
+    }
+    int Close(int fd) override
+    {
+        return oe_syscall(SYS_close, fd);
+    }
 };
 } // namespace
 
+ttls::Dispatcher* dis;
+
+int connect_hook(int sockfd, const sockaddr* addr, socklen_t addrlen)
+{
+    return dis->Connect(sockfd, addr, addrlen);
+}
+
+int send_hook(int sockfd, void* buf, size_t len, int flags)
+{
+    return dis->Send(sockfd, buf, len, flags);
+}
+
+int recv_hook(int sockfd, void* buf, size_t len, int flags)
+{
+    return dis->Recv(sockfd, buf, len, flags);
+}
+
+int shutdown_hook(int fd, int how)
+{
+    return dis->Shutdown(fd, how);
+}
+
+int close_hook(int fd)
+{
+    return dis->Close(fd);
+}
+
 static oe_result_t _syscall_hook(
     long number,
-    long /*arg1*/,
-    long /*arg2*/,
-    long /*arg3*/,
-    long /*arg4*/,
+    long arg1,
+    long arg2,
+    long arg3,
+    long arg4,
     long /*arg5*/,
     long /*arg6*/,
     long* ret)
@@ -48,20 +80,37 @@ static oe_result_t _syscall_hook(
     switch (number)
     {
         case SYS_connect:
-            // TODO call dispatcher instead
-            *ret = 2;
+            *ret = connect_hook(arg1, reinterpret_cast<sockaddr*>(arg2), arg3);
+            return OE_OK;
+        case SYS_write:
+            *ret = send_hook(arg1, reinterpret_cast<void*>(arg2), arg3, arg4);
+            return OE_OK;
+        case SYS_read:
+            *ret = recv_hook(arg1, reinterpret_cast<void*>(arg2), arg3, arg4);
+            return OE_OK;
+        case SYS_shutdown:
+            *ret = shutdown_hook(arg1, arg2);
+            return OE_OK;
+        case SYS_close:
+            *ret = close_hook(arg1);
             return OE_OK;
     }
 
     return OE_UNEXPECTED;
 }
 
+/**
+ * Initialize ttls dispatcher and hook syscalls
+ */
 void ert_init_ttls(const char* config)
 {
     if (!config || !*config)
         return;
 
-    // TODO create dispatcher etc
+    const auto sock = std::make_shared<OESocket>();
+    const auto raw_sock = std::make_shared<ttls::RawSocket>(sock);
+    const auto tls_sock = std::make_shared<ttls::MbedtlsSocket>(sock);
+    dis = new ttls::Dispatcher(config, raw_sock, tls_sock);
 
     oe_register_syscall_hook(_syscall_hook);
 }
