a9ea528af22848136dd4c76c7cd8dfea5b3d3450
Thomas Tendyck <tt@edgeless.systems>
2021-05-19 09:35:36 +0200

    fix OE_OUT_OF_THREADS in ert/libc test
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

---
+++ src/tests/libc/enc/enc.c
@@ -0,0 +1,16 @@
+// same as OE's tests/libc/enc/enc.c, but with 5 instead of 4 NumTCS
+
+#include <openenclave/bits/properties.h>
+
+OE_SET_ENCLAVE_SGX(
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* Debug */
+    512,  /* NumHeapPages */
+    256,  /* NumStackPages */
+    5);   /* NumTCS */
+
+#undef OE_SET_ENCLAVE_SGX
+#define OE_SET_ENCLAVE_SGX(...)
+
+#include "../tests/libc/enc/enc.c"
