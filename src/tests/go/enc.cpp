7d2b33e6f6ffa85f4776c5002d862dd4b3c2945b
Thomas Tendyck <tt@edgeless.systems>
2021-07-06 16:05:36 +0200

    lstat
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/tests/go/enc.cpp
+++ src/tests/go/enc.cpp
@@ -1,10 +1,12 @@
+#include <openenclave/ert.h>
+#include <openenclave/internal/sgx/td.h>
 #include <openenclave/internal/tests.h>
+#include <sys/mount.h>
 #include <iostream>
 #include "test_t.h"
 
-#include <openenclave/internal/sgx/td.h>
-
 using namespace std;
+using namespace ert;
 
 extern "C" const bool ert_enable_signals = true;
 extern "C" const bool ert_enable_sigsegv = true;
@@ -13,6 +15,10 @@ extern "C" int gotest(bool simulate);
 
 void test_ecall()
 {
+    const auto myfs = "myfs";
+    const Memfs memfs(myfs);
+    OE_TEST(mount("/", "/", myfs, 0, nullptr) == 0);
+
     const int r = gotest(oe_sgx_get_td()->simulate);
     cout << "gotest() returned " << r << '\n';
     OE_TEST(r == 42);
