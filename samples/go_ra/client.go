4505f0fc144440608f74a2c4501ebb217adc4881
Thomas Tendyck <51411342+thomasten@users.noreply.github.com>
2021-09-30 09:23:47 +0200

    Update client.go

--- samples/go_ra/client.go
+++ samples/go_ra/client.go
@@ -67,6 +67,8 @@ func verifyReport(reportBytes, certBytes, signer []byte) error {
 		return errors.New("report data does not match the certificate's hash")
 	}
 
+	// You can either verify the UniqueID or the tuple (SignerID, ProductID, SecurityVersion, Debug).
+
 	if report.SecurityVersion < 2 {
 		return errors.New("invalid security version")
 	}
@@ -76,6 +78,8 @@ func verifyReport(reportBytes, certBytes, signer []byte) error {
 	if !bytes.Equal(report.SignerID, signer) {
 		return errors.New("invalid signer")
 	}
+
+	// For production, you must also verify that report.Debug == false
 
 	return nil
 }
