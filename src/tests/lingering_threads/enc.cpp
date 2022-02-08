990f566315758697df9f1b3abd07a0b01cdcd124
Thomas Tendyck <tt@edgeless.systems>
2022-02-08 11:49:29 +0100

    fix regression: lingering thread may crash when not canceled before atexit
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/tests/lingering_threads/enc.cpp
+++ src/tests/lingering_threads/enc.cpp
@@ -1,12 +1,22 @@
 #include <openenclave/internal/tests.h>
+#include <atomic>
 #include <mutex>
 #include <thread>
 #include "test_t.h"
 
 using namespace std;
 
+static atomic<bool*> _invalidated_by_atexit;
+
 void test_ecall()
 {
+    // This is supposed to crash the lingering thread if it is not canceled
+    // before atexit functions are called. If it is canceled shortly after
+    // atexit, the test will be racy and sometimes succeed and sometimes not.
+    bool invalidated_by_atexit = false;
+    _invalidated_by_atexit = &invalidated_by_atexit;
+    OE_TEST(atexit([] { _invalidated_by_atexit = nullptr; }) == 0);
+
     static mutex m;
     m.lock();
     thread([] { m.lock(); }).detach();
@@ -17,7 +27,7 @@ void test_ecall()
     }).detach();
 
     thread([] {
-        for (;;)
+        for (; !*_invalidated_by_atexit;)
             this_thread::sleep_for(1ms);
     }).detach();
 
