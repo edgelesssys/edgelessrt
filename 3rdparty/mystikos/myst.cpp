bf359fbdd2165cbe25a1fcf6c5f2d21c0360e1e2
Thomas Tendyck <tt@edgeless.systems>
2022-06-05 20:30:56 +0200

    update mystikos
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- 3rdparty/mystikos/myst.cpp
+++ 3rdparty/mystikos/myst.cpp
@@ -8,3 +8,21 @@ extern "C" void __myst_panic()
 {
     abort();
 }
+
+extern "C" long myst_syscall_clock_gettime(
+    clockid_t clk_id,
+    struct timespec* tp)
+{
+    *tp = timespec{};
+    return 0;
+}
+
+extern "C" gid_t myst_syscall_getegid()
+{
+    return 0;
+}
+
+extern "C" uid_t myst_syscall_geteuid()
+{
+    return 0;
+}
