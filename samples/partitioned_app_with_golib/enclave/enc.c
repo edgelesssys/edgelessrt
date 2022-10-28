946b7a7052edb68452808967bbb94262fb60d880
Thomas Tendyck <tt@edgeless.systems>
2022-10-28 15:29:01 +0200

    fix missing epoll in golib sample
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- samples/partitioned_app_with_golib/enclave/enc.c
+++ samples/partitioned_app_with_golib/enclave/enc.c
@@ -1,10 +1,16 @@
 // Copyright (c) Edgeless Systems GmbH.
 // Licensed under the MIT License.
 
+#include <stdlib.h>
 #include "../golib/hello.h"
 #include "helloworld_t.h"
 
 void enclave_helloworld()
 {
+    // Go runtime needs epoll
+    if (oe_load_module_host_epoll() != OE_OK)
+        abort();
+
+    // call the exported Go function
     myHello();
 }
