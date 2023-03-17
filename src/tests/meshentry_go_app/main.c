9794a8ff9a4e6523082d6264316532fc0f199a35
Thomas Tendyck <tt@edgeless.systems>
2023-03-17 15:42:21 +0100

    decrease NumHeapPages for Go tests
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/tests/meshentry_go_app/main.c
+++ src/tests/meshentry_go_app/main.c
@@ -9,9 +9,9 @@ int main()
 }
 
 OE_SET_ENCLAVE_SGX(
-    1,      /* ProductID */
-    1,      /* SecurityVersion */
-    true,   /* Debug */
-    131072, /* NumHeapPages */
-    64,     /* NumStackPages */
-    16);    /* NumTCS */
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* Debug */
+    4096, /* NumHeapPages */
+    64,   /* NumStackPages */
+    16);  /* NumTCS */
