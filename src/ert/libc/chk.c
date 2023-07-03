2e1e239bf75f6bdc694acd1256de362073d6f85d
Thomas Tendyck <tt@edgeless.systems>
2023-07-03 11:22:40 +0200

    ubuntu 22.04 support: further changes
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/ert/libc/chk.c
+++ src/ert/libc/chk.c
@@ -9,11 +9,15 @@
 // clang-format off
 #define CHK2(x) void* __##x##_chk(void* a, void* b) { return x(a, b); }
 #define CHK3(x) void* __##x##_chk(void* a, void* b, void* c) { return x(a, b, c); }
+#define CHK4(x) void* __##x##_chk(void* a, void* b, void* c, void* d) { return x(a, b, c, d); }
+#define CHK5(x) void* __##x##_chk(void* a, void* b, void* c, void* d, void* e) { return x(a, b, c, d, e); }
 // clang-format on
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wint-conversion"
 
+CHK5(mbsnrtowcs)
+CHK4(mbsrtowcs)
 CHK3(memcpy)
 CHK3(memmove)
 CHK3(memset)
@@ -27,5 +31,8 @@ CHK2(wcscat)
 CHK2(wcscpy)
 CHK3(wcsncat)
 CHK3(wcsncpy)
+CHK3(wmemcpy)
+CHK3(wmemmove)
+CHK3(wmemset)
 
 #pragma GCC diagnostic pop
