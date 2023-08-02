4be0ebed86f09acdf129920cdbdef57fbe8f0564
Thomas Tendyck <tt@edgeless.systems>
2023-08-02 16:52:09 +0200

    only cancel thread if it's outside the enclave
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/ert/host/enclave_thread_manager.h
+++ src/ert/host/enclave_thread_manager.h
@@ -25,6 +25,11 @@ class EnclaveThreadManager final
     // *func*.
     void create_thread(oe_enclave_t* enclave, void (*func)(oe_enclave_t*));
 
+    // Sets whether it's allowed to call pthread_cancel on the current thread.
+    // This is also a cancelation point (irrespective of the value of
+    // *cancelable*).
+    void set_cancelable(bool cancelable);
+
     // Joins all threads that have been created with create_thread() for
     // *enclave*.
     void join_all_threads(const oe_enclave_t* enclave);
@@ -36,11 +41,20 @@ class EnclaveThreadManager final
   private:
     EnclaveThreadManager() = default;
 
+    enum : unsigned int
+    {
+        Cancelable = 1 << 0,
+        Canceling = 1 << 1,
+    };
+
     struct Thread
     {
         std::thread thread;
         std::atomic<bool> finished;
+        std::atomic<unsigned int> cancel_state;
     };
+
+    static thread_local Thread* self;
 
     std::map<const oe_enclave_t*, std::list<Thread>> threads_;
     std::mutex mutex_;
