bf766b31224f1344e42e89f7c7785fd7cfdb4616
Thomas Tendyck <tt@edgeless.systems>
2026-03-08 20:44:20 +0000

    update go to 1.26.1
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- src/tests/go_ra/enc/enc.cpp
+++ src/tests/go_ra/enc/enc.cpp
@@ -17,6 +17,6 @@ OE_SET_ENCLAVE_SGX(
     1234, /* ProductID */
     2,    /* SecurityVersion */
     true, /* Debug */
-    4096, /* NumHeapPages */
+    8192, /* NumHeapPages */
     64,   /* NumStackPages */
     16);  /* NumTCS */
